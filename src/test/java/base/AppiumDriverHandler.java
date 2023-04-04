ace792bb1b931e8da887eb8e0cd17d65a87e0ad5
gitstanhub <stasdmitruk1@gmail.com>
2023-04-04 02:31:01 +0200

    Added AppiumDriverHandler

--- src/test/java/base/AppiumDriverHandler.java
+++ src/test/java/base/AppiumDriverHandler.java
@@ -1,4 +1,26 @@
 package base;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.net.MalformedURLException;
+
+import static io.appium.java_client.remote.AutomationName.ANDROID_UIAUTOMATOR2;
+import static io.appium.java_client.remote.MobilePlatform.ANDROID;
+
 public class AppiumDriverHandler {
+
+    private AndroidDriver driver;
+
+    public AndroidDriver getDriver() {
+        return driver;
+    }
+
+    public void setUp() throws MalformedURLException {
+        UiAutomator2Options uiAutomator2Options = new UiAutomator2Options()
+                .setApp("src/test/resources/apk/org.wikipedia.apk")
+                .setPlatformName(ANDROID)
+                .setAutomationName(ANDROID_UIAUTOMATOR2);
+    }
 }

908bb8c12cad6335917630ebb97fde48bfb721bf
gitstanhub <stasdmitruk1@gmail.com>
2023-04-05 00:59:20 +0200

    Updated AppiumDriverHandler

--- src/test/java/base/AppiumDriverHandler.java
+++ src/test/java/base/AppiumDriverHandler.java
@@ -1,10 +1,12 @@
 package base;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 
 import static io.appium.java_client.remote.AutomationName.ANDROID_UIAUTOMATOR2;
 import static io.appium.java_client.remote.MobilePlatform.ANDROID;
@@ -17,10 +19,24 @@ public class AppiumDriverHandler {
         return driver;
     }
 
+    @BeforeEach
     public void setUp() throws MalformedURLException {
         UiAutomator2Options uiAutomator2Options = new UiAutomator2Options()
                 .setApp("src/test/resources/apk/org.wikipedia.apk")
                 .setPlatformName(ANDROID)
-                .setAutomationName(ANDROID_UIAUTOMATOR2);
+                .setAutomationName(ANDROID_UIAUTOMATOR2)
+                .setDeviceName("Pixel 6 Pro API 33")
+                .setPlatformVersion("13.0")
+                .setAppPackage("org.wikipedia")
+                .setAppActivity("org.wikipedia.main.MainActivity");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), uiAutomator2Options);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }

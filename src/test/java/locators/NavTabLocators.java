2d750252887ee7dcfe0352fc15c492aea7faa7ea
gitstanhub <stasdmitruk1@gmail.com>
2023-04-10 17:46:45 +0200

    Added steps to onboardingScreenTest for verifying Explore Page and Navigation Tab after closing the Onboarding flow

---
+++ src/test/java/locators/NavTabLocators.java
@@ -0,0 +1,12 @@
+package locators;
+
+public class NavTabLocators {
+
+    public final String NAV_TAB_CONTAINER = "org.wikipedia:id/main_nav_tab_container";
+    public final String NAV_TAB_EXPLORE_BUTTON = "org.wikipedia:id/nav_tab_explore";
+    public final String NAV_TAB_COMMON_BUTTON_LABEL = "org.wikipedia:id/navigation_bar_item_labels_group";
+    public final String NAV_TAB_SAVED_BUTTON = "org.wikipedia:id/nav_tab_reading_lists";
+    public final String NAV_TAB_SEARCH_BUTTON = "org.wikipedia:id/nav_tab_search";
+    public final String NAV_TAB_EDITS_BUTTON = "org.wikipedia:id/nav_tab_edits";
+    public final String NAV_TAB_MORE_BUTTON = "org.wikipedia:id/nav_more_container";
+}
---
+++ src/test/java/pageobjects/elements/NavTabElements.java
@@ -0,0 +1,45 @@
+package pageobjects.elements;
+
+import base.AppiumDriverHandler;
+import io.appium.java_client.android.AndroidDriver;
+import locators.NavTabLocators;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class NavTabElements extends AppiumDriverHandler {
+
+    private final AndroidDriver driver;
+    private final NavTabLocators navTabLocators = new NavTabLocators();
+
+    public NavTabElements() {
+        this.driver = getDriver();
+    }
+
+    public WebElement getNavTab() {
+        return driver.findElement(By.id(navTabLocators.NAV_TAB_CONTAINER));
+    }
+
+    public WebElement getExploreButton() {
+       return driver.findElement(By.id(navTabLocators.NAV_TAB_EXPLORE_BUTTON));
+    }
+
+    public WebElement getSavedButton() {
+        return driver.findElement(By.id(navTabLocators.NAV_TAB_SAVED_BUTTON));
+    }
+
+    public WebElement getSearchButton() {
+        return driver.findElement(By.id(navTabLocators.NAV_TAB_SEARCH_BUTTON));
+    }
+
+    public WebElement getEditsButton() {
+        return driver.findElement(By.id(navTabLocators.NAV_TAB_EDITS_BUTTON));
+    }
+
+    public WebElement getMoreButton() {
+        return driver.findElement(By.id(navTabLocators.NAV_TAB_MORE_BUTTON));
+    }
+
+    public WebElement getNavTabButtonLabel() {
+        return driver.findElement(By.id(navTabLocators.NAV_TAB_COMMON_BUTTON_LABEL));
+    }
+}

50475140caf55bc28fc8f0225441a0cef01e4fbb
gitstanhub <stasdmitruk1@gmail.com>
2023-04-16 04:56:57 +0200

    Updated config variables for the real device host

--- src/test/java/base/AppiumDriverHandler.java
+++ src/test/java/base/AppiumDriverHandler.java
@@ -26,39 +26,42 @@ public class AppiumDriverHandler {
 
         switch (deviceHost) {
             case "emulator":
-                desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/src/test/resources/apk/org.wikipedia.apk");
+                desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + androidConfig.appRootPath());
                 desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, ANDROID);
                 desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, ANDROID_UIAUTOMATOR2);
-                desiredCapabilities.setCapability(MobileCapabilityType.UDID, "emulator-5554");
-                desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 6 Pro API 33");
-                desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0");
-                desiredCapabilities.setCapability("appPackage", "org.wikipedia");
-                desiredCapabilities.setCapability("appActivity", "org.wikipedia.main.MainActivity");
-                driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), desiredCapabilities);
+                desiredCapabilities.setCapability(MobileCapabilityType.UDID, androidConfig.udid());
+                desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, androidConfig.deviceName());
+                desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, androidConfig.platformVersion());
+                desiredCapabilities.setCapability("appPackage", androidConfig.appPackage());
+                desiredCapabilities.setCapability("appActivity", androidConfig.appActivity());
+                driver = new AndroidDriver(new URL(androidConfig.remoteUrl()), desiredCapabilities);
+                System.out.println("Emulator is selected");
                 break;
 
             case "realdevice":
-                desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/src/test/resources/apk/org.wikipedia.apk");
+                desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + androidConfig.appRootPath());
                 desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, ANDROID);
                 desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, ANDROID_UIAUTOMATOR2);
-                desiredCapabilities.setCapability(MobileCapabilityType.UDID, "01021FFBA000PH");
-                desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 4 XL Android 13");
-                desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0");
-                desiredCapabilities.setCapability("appPackage", "org.wikipedia");
-                desiredCapabilities.setCapability("appActivity", "org.wikipedia.main.MainActivity");
-                driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), desiredCapabilities);
+                desiredCapabilities.setCapability(MobileCapabilityType.UDID, androidConfig.udid());
+                desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, androidConfig.deviceName());
+                desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, androidConfig.platformVersion());
+                desiredCapabilities.setCapability("appPackage", androidConfig.appPackage());
+                desiredCapabilities.setCapability("appActivity", androidConfig.appActivity());
+                driver = new AndroidDriver(new URL(androidConfig.remoteUrl()), desiredCapabilities);
+                System.out.println("Real device is selected");
                 break;
 
             case "browserstack":
-                desiredCapabilities.setCapability("deviceName", "Google Pixel 7 Pro");
-                desiredCapabilities.setCapability("platformVersion", "13.0");
-                desiredCapabilities.setCapability("platformName", "android");
-                desiredCapabilities.setCapability("project", "Experiment UI Automation project - Wiki Android");
-                desiredCapabilities.setCapability("build", "Android test build");
-                desiredCapabilities.setCapability("name", "OnboardingScreenTest");
-                desiredCapabilities.setCapability("browserstack.debug", true);
-                desiredCapabilities.setCapability("app", "app_url");
+                desiredCapabilities.setCapability("deviceName", "");
+                desiredCapabilities.setCapability("platformVersion", "");
+                desiredCapabilities.setCapability("platformName", "");
+                desiredCapabilities.setCapability("project", "");
+                desiredCapabilities.setCapability("build", "");
+                desiredCapabilities.setCapability("name", "");
+                desiredCapabilities.setCapability("browserstack.debug", "");
+                desiredCapabilities.setCapability("app", "");
                 driver = new AndroidDriver(new URL("http://" + "username" + ":" + "access" + "@" + "hub-cloud.browserstack.com" + "/wd/hub"), desiredCapabilities);
+                System.out.println("Browserstack is selected");
                 break;
 
             default:
--- src/test/java/config/AndroidDriverConfig.java
+++ src/test/java/config/AndroidDriverConfig.java
@@ -13,5 +13,45 @@ public interface AndroidDriverConfig extends Config {
     @Key("deviceHost")
     String deviceHost();
 
+    @Key("deviceName")
+    String deviceName();
 
+    @Key("platformVersion")
+    String platformVersion();
+
+    @Key("udid")
+    String udid();
+
+    @Key("appRootPath")
+    String appRootPath();
+
+    @Key("appPackage")
+    String appPackage();
+
+    @Key("appActivity")
+    String appActivity();
+
+    @Key("buildName")
+    String buildName();
+
+    @Key("remoteUrl")
+    String remoteUrl();
+
+    @Key("projectName")
+    String projectName();
+
+    @Key("testRunName")
+    String testRunName();
+
+    @Key("browserStackDebug")
+    boolean browserStackDebug();
+
+    @Key("browserStackAppUrl")
+    String browserStackAppUrl();
+
+    @Key("browserStackUsername")
+    String browserStackUsername();
+
+    @Key("browserStackAccessToken")
+    String browserStackAccessToken();
 }
